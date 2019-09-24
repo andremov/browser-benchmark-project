1feb8be13fabe1731518f043dc37241a279d0ff3
Andres Movilla <andres_movilla1103@hotmail.com>
2019-09-23 22:49:21 -0500

    build 1.2

---
+++ browserbenchmark/build/precache-manifest.2ad4de2712a372b2942ca5c0f50ed921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0edd34c50e00b467d2354cb96ce046a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23a0f5b06c8248052620",
+    "url": "/static/css/main.eec9355d.chunk.css"
+  },
+  {
+    "revision": "569efd22b9a39855a0ad",
+    "url": "/static/js/2.8a92b1e2.chunk.js"
+  },
+  {
+    "revision": "23a0f5b06c8248052620",
+    "url": "/static/js/main.751f91cb.chunk.js"
+  },
+  {
+    "revision": "cef9be10f5b8d74b2807",
+    "url": "/static/js/runtime-main.052f397a.js"
+  }
+]);
--- browserbenchmark/build/service-worker.js
+++ browserbenchmark/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6588cbfec87a2594140beac883fe7d51.js"
+  "/precache-manifest.2ad4de2712a372b2942ca5c0f50ed921.js"
 );
 
 self.addEventListener('message', (event) => {
